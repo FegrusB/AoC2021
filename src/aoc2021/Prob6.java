4cfdf0ffef7c92225d8c3449cc126e660af7a505
FegrusB <fergus@beckerleg.co.uk>
2022-11-16 17:03:39 +0000

    added calculation for intital solve. Works for test Input

--- src/aoc2021/Prob6.java
+++ src/aoc2021/Prob6.java
@@ -9,15 +9,32 @@ public class Prob6 {
 
 	public static void main(String[] args) {
 		
+		//Read in starting fish to arraylist
 		Scanner myScanner = GetScanner.get("2021-6a.txt");
 		String[] line = myScanner.nextLine().split(",");
-		int[] startFish = Arrays.stream(line).mapToInt(Integer::parseInt).toArray();
-		//ArrayList<Integer> fish = new ArrayList<>(Arrays.stream(startFish).boxed().toList());
-		
-		
 		ArrayList<Integer> fish = new ArrayList<>(Arrays.stream(line).mapToInt(Integer::parseInt).boxed().toList());
-		System.out.println();
 		
+		System.out.println(play1(fish,80));
+	}
+	public static int play1(ArrayList<Integer> fish, int days) {
+	
+		//loop for days and each Integer in fish. Reset addFish each loop
+		for(int i = 0; i<days;i++) {
+			int addFish = 0;
+			for(int x = 0; x < fish.size();x++) {
+				
+				//if fish is 0, reset to 6 and increment add fish, else decrement
+				if( fish.get(x) == 0) {addFish ++; fish.set(x, 6);}
+				else {fish.set(x,(fish.get(x)-1));}
+			
+			}
+			
+			//add one fish at age 8 for each addFish
+			for(int a = 0;a<addFish;a++) {fish.add(8);}
+		}
+		
+		//return final num of fish
+		return fish.size();
 	}
 
 }

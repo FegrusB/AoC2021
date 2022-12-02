09dc2c5fbfbfda99d90ccc8a9252ce3b189be0b9
FegrusB <fergus@beckerleg.co.uk>
2022-12-02 18:08:29 +0000

    started solve for 9.2

--- src/aoc2021/Prob9.java
+++ src/aoc2021/Prob9.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 
 public class Prob9 {
 
-    static final int xBounds = 100;
-    static final int yBounds = 100;
+    static final int xBounds = 10;
+    static final int yBounds = 5;
 
     public static void main(String[] args){
 
         //Read in starting nums into int array.
-        Scanner myScanner = GetScanner.get("2021-9.txt");
+        Scanner myScanner = GetScanner.get("2021-9a.txt");
         int[][] board = new int[xBounds+2][yBounds+2];
         int[][] boardIn = new int[xBounds][yBounds];
-        ///boolean[][] bottom = new boolean[xBounds][yBounds];
+        boolean[][] basinBool = new boolean[xBounds+2][yBounds+2];
         int count = 0;
         while (myScanner.hasNext()) {
             int[] row = Arrays.stream(myScanner.nextLine().split("")).mapToInt(Integer::parseInt).toArray();
@@ -27,16 +27,29 @@ public class Prob9 {
             System.arraycopy(boardIn[i - 1], 0, board[i], 1, yBounds);
         }
 
-        int sum = 0;
+        for (int x  =1; x<xBounds + 2;x++) {
+            for (int y = 1; y < yBounds + 2; y++) {
+                if(board[x][y]>9){basinBool[x][y] = true;}
+            }
+        }
+
+        ArrayList<Integer> basins = new ArrayList<>();
         for (int x  =1; x<xBounds + 1;x++){
             for(int y=1; y<yBounds + 1;y++){
 
-                if((board[x][y-1] > board[x][y])&&(board[x][y+1] > board[x][y])&&(board[x-1][y] > board[x][y])&&(board[x+1][y] > board[x][y])){ sum += 1 + board[x][y];}
+                if((board[x][y-1] > board[x][y])&&(board[x][y+1] > board[x][y])&&(board[x-1][y] > board[x][y])&&(board[x+1][y] > board[x][y])){
+
+
+
+                }
 
             }
         }
 
+        int sum = 0;
+
         System.out.println(sum);
 
     }
+
 }

fdfca2c37f1478312928634059a70c07e935f443
fergu <fergus@beckerleg.co.uk>
2022-12-16 21:31:20 +0000

    16/12/2022 solve for 11.1, recursive flash

--- src/aoc2021/Prob11.java
+++ src/aoc2021/Prob11.java
@@ -1,6 +1,8 @@
 package aoc2021;
 
 
+import java.util.Arrays;
+
 public class Prob11 {
 
     static final int xBounds = 10;
@@ -9,28 +11,65 @@ public class Prob11 {
     public static void main(String[] args){
 
         //Build board of ints with boundary of 0s
-        board board =  new board(xBounds,yBounds);
+        Board11 board =  new Board11(xBounds,yBounds);
+        int steps = 100;
+        int count = 0;
 
-        System.out.println();
+        for(int i = 0; i < steps ; i++){
+            board.resetChanged();
+            for(int x = 1; x < xBounds + 1; x ++){
+
+                for( int y = 1; y < yBounds+1; y++){
+                    int[] pos = {x, y};
+                    if(board.getPos(pos) == 0){if(board.getChanged(pos)){continue;}}
+                    board.incPos(pos);
+                    if( board.getPos(pos) > 9){ count = count +  flash(board,pos);}
+
+                }
+
+            }
 
 
+        }
+        System.out.println(count);
+    }
+    public static int flash(Board11 board, int[] pos){
+
+        int countF = 1;
+        board.zeroPos(pos);
+        board.setChanged(pos);
+
+        for(int xD = -1; xD < 2; xD ++){
+            for(int yD = -1; yD < 2; yD++){
+                int[] newPos = {pos[0] + xD,pos[1]+yD};
+                int val = board.getPos(newPos);
+                if(val == -1 || (val == 0 && board.getChanged(newPos) )){continue;}
+                board.incPos(newPos);
+                if (board.getPos(newPos)>9){board.zeroPos(newPos); countF += flash(board,newPos);}
+            }
+
+        }
+        return countF;
     }
 }
-class board{
+class Board11 {
 
-    private int[][] board;
+    private final int[][] board;
+    private final boolean[][] changed;
 
-    public board(int xBounds, int yBounds){
+    public Board11(int xBounds, int yBounds){
 
-        this.board = boardWithBoundaries.build("2021-11a.txt",xBounds,yBounds,0);
-
+        this.board = boardWithBoundaries.build("2021-11.txt",xBounds,yBounds, -1);
+        this.changed = new boolean[xBounds][yBounds];
     }
 
-    public int getPos(int[] pos) {
-        return board[pos[0]][pos[1]];
+    public int getPos(int[] pos) {return board[pos[0]][pos[1]];}
+    //public void setPos(int valIn, int[] pos) {this.board[pos[0]][pos[1]] = valIn;}
+    public void incPos(int[] pos){this.board[pos[0]][pos[1]]++;}
+    public void zeroPos(int[] pos){this.board[pos[0]][pos[1]] = 0;}
+    public void resetChanged(){
+        for(int i = 0; i < Prob11.xBounds; i ++)Arrays.fill(this.changed[i],false);
     }
-
-    public void setPos(int valIn, int[] pos) {
-        this.board[pos[0]][pos[1]] = valIn;
-    }
+    public void setChanged(int[] pos){this.changed[pos[0] - 1][pos[1] - 1] = true;}
+    public boolean getChanged(int[] pos){return this.changed[pos[0] - 1][pos[1] -1];}
 }
